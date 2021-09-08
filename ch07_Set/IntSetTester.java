d7c4c3b80c0e12ff28abb46bafe1320c5fc7ea51
Da-innisfree <dainni.k@gmail.com>
2021-09-08 18:30:47 +0900

    Set

---
+++ ch07_Set/IntSetTester.java
@@ -0,0 +1,27 @@
+package ch07_Set;
+
+//int형 집합 클래스 IntSet을 사용하는 예
+public class IntSetTester {
+    public static void main(String[] args) {
+        IntSet s1 = new IntSet(20);
+        IntSet s2 = new IntSet(20);
+        IntSet s3 = new IntSet(20);
+
+        s1.add(10);
+        s1.add(15);
+        s1.add(20);
+        s1.add(25);
+
+        s1.copyTo(s2);
+        s2.add(12);
+        s2.remove(25);
+
+        s3.copyFrom(s2);
+
+        System.out.println("s1= "+s1);
+        System.out.println("s2= "+s2);
+        System.out.println("s3= "+s3);
+
+
+    }
+}
---
+++ ch07_Set/ToStringTester.java
@@ -0,0 +1,4 @@
+package ch07_Set;
+
+public class ToStringTester {
+}
